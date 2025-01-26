829ae5b6bb5318701761de76616da0e0e52b552d
ANISHSINGH0110 <anish2003s@gmail.com>
2025-01-26 17:32:20 +0530

    added approach in 1st  dsa question

--- 26_01_25/DSA/2127_LeetCode.cpp
+++ 26_01_25/DSA/2127_LeetCode.cpp
@@ -1,3 +1,20 @@
+// Approach
+
+/*
+For this problem, we can find the length of the maximum cycle in the graph. Here we only need to find the length of the largest cycle, because if there are multiple cycles, they are not connected to each other, which does not meet the problem requirements.
+
+In addition, for the size of the cycle equal to 
+2
+, that is, there are two employees who like each other, then we can arrange these two employees together. If these two employees are each liked by other employees, then we only need to arrange the employees who like them next to them. If there are multiple such situations, we can arrange them all.
+
+Therefore, the problem is actually equivalent to finding the length of the maximum cycle in the graph, and all cycles of length 
+2
+ plus their longest chain. The maximum of these two can be found. To find the longest chain to the cycle of length 
+2
+, we can use topological sorting.
+The time complexity is O(n)
+*/
+
 class Solution {
 public:
      int topologicalSort(vector<int>& fa) {
